01c2a1118211af16dc3f0c44f97637b89d7221c2
Evgeny Kapranov <e.s.kapranov@yandex.ru>
2022-05-25 18:31:17 +0300

    Initial commit

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ class Student:
                 lector.grades[course] = [grade]
         else:
             return 'Ошибка'
+    def avg_grade(self):
+        summ, count = 0, 0
+        for theme in self.grades.values():
+            for i in theme:
+                summ += i
+                count += 1
+        return round(summ / count, 1)
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname}\nСредняя оценка за домашние задания:{self.avg_grade()}' \
+              f'\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
+              f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        return res
 
 class Mentor:
     def __init__(self, name, surname):
@@ -29,8 +42,15 @@ class Lecturer(Mentor):
         self.courses_attached = []
 
     def avg_grade(self):
-        for theme in value(self.grades):
+        summ, count = 0, 0
+        for theme in self.grades.values():
             for i in theme:
+                summ += i
+                count += 1
+        return round(summ / count, 1)
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname}\nСредняя оценка за лекции:{self.avg_grade()}'
+        return res
 
 
 class Reviewer(Mentor):
@@ -48,7 +68,9 @@ class Reviewer(Mentor):
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
-
+best_student.courses_in_progress += ['Git']
+best_student.finished_courses += ['Введение в программирование']
+best_student.finished_courses += ['Английский для программирования']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
@@ -64,6 +86,8 @@ cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
-print(nice_lector.grades)
-print(cool_reviewer)
+# print(best_student.grades)
+# print(nice_lector.grades)
+# print(cool_reviewer)
+# print(nice_lector)
+print(best_student)
