5c3a285290cac59100dc63d490411bbc96af2135
Evgeny Kapranov <e.s.kapranov@yandex.ru>
2022-05-25 17:34:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lec(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self,name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        self.courses_attached = []
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+nice_lector = Lecturer('Bob', 'Hunter')
+nice_lector.courses_attached += ['Python']
+
+best_student.rate_lec(nice_lector, 'Python', 10)
+best_student.rate_lec(nice_lector, 'Python', 10)
+best_student.rate_lec(nice_lector, 'Python', 9)
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
+print(nice_lector.grades)
